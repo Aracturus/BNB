68b4801717d567d1b321a79c4b15b7eeeb8d0677
Swati Mandal <91366354+Aracturus@users.noreply.github.com>
2021-12-01 18:37:19 +0530

    done

---
+++ index.js
@@ -0,0 +1,8 @@
+function handelclick() {
+    var x = document.getElementById("cd");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
